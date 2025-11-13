1f87763e0fa8e4ecc6f4a9fce2f0a21d5b7c5eb7
hitesharma05 <sharmahitesh28792@gmail.com>
2025-11-13 16:49:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navbar color change on scroll
+window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
